b2c5b748980558cdcc35561848b69445f2b2daa5
Balamurali03 <balamurali03101997@gmail.com>
2024-07-08 17:58:29 +0530

    Frontend Code complete commit

---
+++ qrapp/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import QR_Generator from './Component/QR_Generator';
+function App() {
+  return (
+    <div className="App">
+     <QR_Generator></QR_Generator>
+     </div>
+  );
+}
+
+export default App;
